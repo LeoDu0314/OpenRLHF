181dd138a45415d7c1bf3e74234da00777f48e5b
leodu <lxdu0314@gmail.com>
2025-03-05 17:17:32 +0800

    fix eval code

--- eval/k12/evaluate_k12.py
+++ eval/k12/evaluate_k12.py
@@ -54,7 +54,7 @@ def evaluate_chat_model():
                         "role": "system",
                         "content": "你是书生·万象，英文名是InternVL，是由上海人工智能实验室、清华大学及多家合作单位联合开发的多模态大语言模型。",
                     },
-                    {"role": "user", "content": data_item["query"]},
+                    {"role": "user", "content": "<image>\n" + question},
                 ]
             elif args.prompt_template == "reasoning_instruct":
                 messages = [
