2ed0d878d454dab4da78db8987a7915898523e0d
leodu <lxdu0314@gmail.com>
2025-03-05 16:52:08 +0800

    fix eval code

--- eval/mathverse/evaluate_mathverse.py
+++ eval/mathverse/evaluate_mathverse.py
@@ -26,13 +26,39 @@ def evaluate_chat_model():
         for idx, data_item in tqdm(enumerate(data)):
             data_item["query"] = data_item["query_cot"]
             image = data_item["image"]
-            messages = [
-                {
-                    "role": "system",
-                    "content": "你是书生·万象，英文名是InternVL，是由上海人工智能实验室、清华大学及多家合作单位联合开发的多模态大语言模型。",
-                },
-                {"role": "user", "content": "<image>\n" + data_item["query"]},
-            ]
+            if args.prompt_template == "original":
+                messages = [
+                    {
+                        "role": "system",
+                        "content": "你是书生·万象，英文名是InternVL，是由上海人工智能实验室、清华大学及多家合作单位联合开发的多模态大语言模型。",
+                    },
+                    {"role": "user", "content": "<image>\n" + data_item["query"]},
+                ]
+            elif args.prompt_template == "reasoning_instruct":
+                messages = [
+                    {
+                        "role": "system",
+                        "content": "你是书生·万象，英文名是InternVL，是由上海人工智能实验室、清华大学及多家合作单位联合开发的多模态大语言模型。",
+                    },
+                    {
+                        "role": "user",
+                        "content": "<image>\nYou should first thinks about the reasoning process in the mind and then provides the user with the answer. Your answer must be in latex format and wrapped in $...$.The reasoning process and answer are enclosed within <think> </think> and <answer> </answer> tags, respectively, i.e., <think> Since $1+1=2$, so the answer is $2$. </think><answer> $2$ </answer>, which means your output should start with <think> and end with </answer>.\n"
+                        + data_item["query"],
+                    },
+                ]
+            elif args.prompt_template == "reasoning_pretrain":
+                messages = [
+                    {
+                        "role": "system",
+                        "content": "A conversation between User and Assistant. The user asks a question, and the Assistant solves it. The assistant first thinks about the reasoning process in the mind and then provides the user with the answer. The reasoning process and answer are enclosed within <think> </think> and <answer> </answer> tags, respectively, i.e., <think> reasoning process here </think> <answer> answer here </answer>.",
+                    },
+                    {
+                        "role": "user",
+                        "content": f"<image>\nAnswer the following question: {data_item['query']}\nPlease reason step by step, and put your final answer within \\boxed{{}}.",
+                    },
+                ]
+            else:
+                raise ValueError(f"Invalid prompt template: {args.prompt_template}")
             prompt = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
             inputs.append(
                 {
--- eval/olympiadbench/evaluate_olympiadbench.py
+++ eval/olympiadbench/evaluate_olympiadbench.py
@@ -25,13 +25,39 @@ def evaluate_chat_model():
         inputs = []
         for idx, data_item in tqdm(enumerate(data)):
             data_item["query"] = re.sub(r"<image_\d+>", "<image>", data_item["question"])
-            messages = [
-                {
-                    "role": "system",
-                    "content": "你是书生·万象，英文名是InternVL，是由上海人工智能实验室、清华大学及多家合作单位联合开发的多模态大语言模型。",
-                },
-                {"role": "user", "content": data_item["query"]},
-            ]
+            if args.prompt_template == "original":
+                messages = [
+                    {
+                        "role": "system",
+                        "content": "你是书生·万象，英文名是InternVL，是由上海人工智能实验室、清华大学及多家合作单位联合开发的多模态大语言模型。",
+                    },
+                    {"role": "user", "content": data_item["query"]},
+                ]
+            elif args.prompt_template == "reasoning_instruct":
+                messages = [
+                    {
+                        "role": "system",
+                        "content": "你是书生·万象，英文名是InternVL，是由上海人工智能实验室、清华大学及多家合作单位联合开发的多模态大语言模型。",
+                    },
+                    {
+                        "role": "user",
+                        "content": "You should first thinks about the reasoning process in the mind and then provides the user with the answer. Your answer must be in latex format and wrapped in $...$.The reasoning process and answer are enclosed within <think> </think> and <answer> </answer> tags, respectively, i.e., <think> Since $1+1=2$, so the answer is $2$. </think><answer> $2$ </answer>, which means your output should start with <think> and end with </answer>.\n"
+                        + data_item["query"],
+                    },
+                ]
+            elif args.prompt_template == "reasoning_pretrain":
+                messages = [
+                    {
+                        "role": "system",
+                        "content": "A conversation between User and Assistant. The user asks a question, and the Assistant solves it. The assistant first thinks about the reasoning process in the mind and then provides the user with the answer. The reasoning process and answer are enclosed within <think> </think> and <answer> </answer> tags, respectively, i.e., <think> reasoning process here </think> <answer> answer here </answer>.",
+                    },
+                    {
+                        "role": "user",
+                        "content": f"Answer the following question: {data_item['query']}\nPlease reason step by step, and put your final answer within \\boxed{{}}.",
+                    },
+                ]
+            else:
+                raise ValueError(f"Invalid prompt template: {args.prompt_template}")
             prompt = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
             if "image_1" in data_item:
                 image = []
@@ -85,6 +111,12 @@ if __name__ == "__main__":
     parser.add_argument("--datasets", type=str, default="")
     parser.add_argument("--out-dir", type=str, default="results")
     parser.add_argument("--seed", type=int, default=0)
+    parser.add_argument(
+        "--prompt_template",
+        type=str,
+        choices=["original", "reasoning_instruct", "reasoning_pretrain"],
+        default="original",
+    )
     args = parser.parse_args()
 
     if not os.path.exists(args.out_dir):
